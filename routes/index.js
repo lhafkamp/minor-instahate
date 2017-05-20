c80a73daa2e92b42d62473072c1d0ddc331e4e49
lhafkamp <luukhafkamp@gmail.com>
2017-05-21 00:13:02 +0200

    :hammer: changed imageController to mainController for clarity

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const userController = require('../controllers/userController')
-const imageController = require('../controllers/imageController')
+const mainController = require('../controllers/mainController')
 const ratingController = require('../controllers/ratingController')
 
 router.get('/', userController.homePage)
 router.get('/succes', userController.authSucces)
 
-router.get('/main', imageController.mainPage)
+router.get('/main', mainController.mainPage)
 router.post('/main/:id/rating', ratingController.rating)
 
 module.exports = router
